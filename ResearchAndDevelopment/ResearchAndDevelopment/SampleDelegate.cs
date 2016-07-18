0aba43a93b77266e22e2894777244e852a164156
ak9779984 <ak9779984@gmail.com>
2016-07-18 08:35:13 +0530

    Missing Changes

---
+++ ResearchAndDevelopment/ResearchAndDevelopment/SampleDelegate.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResearchAndDevelopment
+{
+    class SampleDelegate
+    {
+        public delegate int MathOperations(int a, int b);
+        //public MathOperations myMathDelegate = null;
+
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Sub(int a, int b)
+        {
+            return a - b;
+        }
+
+        public MathOperations GetAddorSubDelegate(bool isAddDelegate)
+        {
+            MathOperations obj = null;
+
+            if (isAddDelegate)
+            {
+                return obj = Add;
+            }
+            else
+            {
+                return obj = Sub;
+            }
+        }
+    }
+}
