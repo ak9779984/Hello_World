8d03149702a9eb42ad8dc8b8f3ec7d2859f422b5
ak9779984 <ak9779984@gmail.com>
2016-07-18 08:23:26 +0530

    Delegate Changes

--- ResearchAndDevelopment/ResearchAndDevelopment/Program.cs
+++ ResearchAndDevelopment/ResearchAndDevelopment/Program.cs
@@ -10,6 +10,29 @@ namespace ResearchAndDevelopment
     {
         static void Main(string[] args)
         {
+            SampleDelegate obj = new SampleDelegate();
+            SampleDelegate.MathOperations objAdd = null;
+
+            List<int> list = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            int addResult = 0;
+            int subResult = 0;
+            for (int i = 0; i < list.Count; i++)
+            {
+                //a = obj.Add(a, list[i]);
+                //a = objAdd(a, list[i]);
+                //a = obj.GetAddorSubDelegate(true).Invoke(a, list[i]);
+
+                objAdd = obj.GetAddorSubDelegate(true);
+                objAdd += obj.GetAddorSubDelegate(false);
+
+                addResult = objAdd.Invoke(1, 2);
+            }
+
+            //Console.WriteLine(a);
+            Console.WriteLine("Add Result : " + addResult);
+            //Console.WriteLine(a);
+            Console.Read();
         }
     }
 }
