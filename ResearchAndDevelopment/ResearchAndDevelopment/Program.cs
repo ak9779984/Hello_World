df13ee9bb82db1d2a62e0e97547c32c8873ee740
ak9779984 <ak9779984@gmail.com>
2016-07-19 08:07:03 +0530

    Events Info

--- ResearchAndDevelopment/ResearchAndDevelopment/Program.cs
+++ ResearchAndDevelopment/ResearchAndDevelopment/Program.cs
@@ -10,29 +10,60 @@ namespace ResearchAndDevelopment
     {
         static void Main(string[] args)
         {
-            SampleDelegate obj = new SampleDelegate();
-            SampleDelegate.MathOperations objAdd = null;
+            #region Custom Event
 
-            List<int> list = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            SampleEventClass objEvent = new SampleEventClass();
+            objEvent.StrValue = "My Test Result";
+            Console.WriteLine("Before Value Change : "+objEvent.StrValue);
+            //SampleEventClass.StringValueChangeDelegate objDelegate = new SampleEventClass.StringValueChangeDelegate(objEvent_StringValueChanged);
 
-            int addResult = 0;
-            int subResult = 0;
-            for (int i = 0; i < list.Count; i++)
-            {
-                //a = obj.Add(a, list[i]);
-                //a = objAdd(a, list[i]);
-                //a = obj.GetAddorSubDelegate(true).Invoke(a, list[i]);
+            objEvent.StringValueChanged += new SampleEventClass.StringValueChangeDelegate(objEvent_StringValueChanged);
 
-                objAdd = obj.GetAddorSubDelegate(true);
-                objAdd += obj.GetAddorSubDelegate(false);
+            //objEvent.StingValueChanged += new SampleEventClass.StingValueChanged();
 
-                addResult = objAdd.Invoke(1, 2);
-            }
+            objEvent.StrValue = "My Test Result";//"Test Result Changed";
+            Console.WriteLine("After Value Change : " + objEvent.StrValue);
 
+            #endregion
+
+            #region Delegates
+            //SampleDelegate obj = new SampleDelegate();
+            //SampleDelegate.MathOperations objAdd = null;
+
+            //List<int> list = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            //int addResult = 0;
+            //int subResult = 0;
+            //for (int i = 0; i < list.Count; i++)
+            //{
+            //    //a = obj.Add(a, list[i]);
+            //    //a = objAdd(a, list[i]);
+            //    //a = obj.GetAddorSubDelegate(true).Invoke(a, list[i]);
+
+            //    objAdd = obj.GetAddorSubDelegate(true);
+            //    objAdd += obj.GetAddorSubDelegate(false);  
+
+            //    bool getSubMethod = false;
+            //    if (!getSubMethod)
+            //    {
+            //        objAdd -= obj.GetAddorSubDelegate(false);  
+            //    }                
+
+            //    addResult = objAdd.Invoke(1, 2);
+            //}
+
+            ////Console.WriteLine(a);
+            //Console.WriteLine("Add Result : " + addResult);
             //Console.WriteLine(a);
-            Console.WriteLine("Add Result : " + addResult);
-            //Console.WriteLine(a);
+
+            #endregion
+
             Console.Read();
+        }
+
+        private static void objEvent_StringValueChanged()
+        {
+            Console.WriteLine("Test Changed Successfully.");
         }
     }
 }
---
+++ ResearchAndDevelopment/ResearchAndDevelopment/SampleEventClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResearchAndDevelopment
+{
+    public class SampleEventClass
+    {
+        public delegate void StringValueChangeDelegate();
+
+        public event StringValueChangeDelegate StringValueChanged;
+
+        string _strValue;
+
+        public string StrValue
+        {
+            get
+            {
+                return _strValue;
+            }
+            set
+            {
+                _strValue = value;
+
+                if (StringValueChanged != null)
+                {
+                    StringValueChanged();
+                }
+            }
+        }
+
+
+    }
+}
